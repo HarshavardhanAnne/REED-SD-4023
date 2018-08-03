d8e06c7e61e194a3f868aeb16c6597f4e631edde
HarshavardhanAnne <harshanne1996@gmail.com>
2018-08-03 13:33:00 -0400

    Removed commenting in sd_4023.py. Created README.md

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# REED-SD-4023
+# REED SD-4023 Sound Meter RS232 Communication Module
+
+This is a python2.7 module for the SD-4023 sound level meter/datalogger. The user manual
+this module was written for can be found at:
+[http://www.reedinstruments.com/pdfs/cache/www.reedinstruments.com/sd-4023/manual/sd-4023-manual.pdf](http://www.reedinstruments.com/pdfs/cache/www.reedinstruments.com/sd-4023/manual/sd-4023-manual.pdf)
+
+This module is intended for use on Linux distributions, and has only been tested on Debian based distros using python 2.7.
+
+## Getting Started
+
+The REED SD-4023 Sound Level Meter is a device that measures sound levels nearby in decibel units. The SD-4023 communicates data via two methods: sd card and RS232 serial. The SD-4023 writes a .xls file to the sd card. This python module focuses on the RS232 serial communication and how to receive data from the unit.
+
+## Prerequisites
+
+### Materials
+
+* 3.5mm TRS Stereo Jack
+* DB9 Female Breakout Board
+* RS232 to USB Adapter
+
+### Hardware
+
+On page 14 of the user manual, the PC Serial Interface shows an invalid wiring pinout. In order to properly interface with the SD-4023 using RS232 serial communication, the following connections must be made:
+* 3.5mm TRS jack tip/top (left) goes to pin 4 DTR on the DB9 breakout
+* 3.5mm TRS jack sleeve (ground) goes to pin 2 RX on the DB9 breakout
+
+![alt text](wiring_diagram.jpg)
+
+### Software
+
+Clone this repository using SSH(or download the .zip file)
+
+```
+git clone git@github.com:HarshavardhanAnne/REED-SD-4023.git
+```
+
+Install the python2.7 serial library
+
+```
+sudo apt-get install python-serial
+```
+
+Navigate to the cloned directory and copy sd_4023.py to the project directory you are working in
+
+```
+cp sd_4023.py /path/of/project/
+```
+
+## How to Use
+
+To use this module, the following inputs are required when creating an instance of this class:
+* PORT: Linux serial port the unit is connected to, usually '/dev/ttyUSB0'
+* PRINT_OPTION (optional): Enables or disables debug print statements (0 = Disabled, 1 = Enabled)
+
+## Example
+
+from sd_4023 import SD_4023
+sdObject1 = SD_4023('/dev/ttyUSB0')
+\#sdObject2 = SD_4023('/dev/ttyUSB0',1)
+
+\#Start serial connection to device
+sdObject1.open()
+
+\#Read entire 16 digit data stream, returned as a list
+data_stream = sdObject1.read()
+print data_stream
+
+\#Read only the decibel level, returned as a float
+decibels = sdObject1.read_decibel()
+print decibels
+
+\#Get the status of the serial connection
+sta = sdObject1.get_status()
+print sta
+
+\#Close serial connection to device
+sdObject1.close()
---
+++ sd_4023.py
@@ -0,0 +1,101 @@
+#Version 0.2
+#Author: Harshavardhan Anne
+
+import serial
+
+class SD_4023(object):
+    _baudrate = 9600
+    _port = '/dev/ttyUSB0'
+    _serialObj = serial.Serial()
+    _print_option = 0   #0 = No printing, 1 = console printing
+    _data_buffer = [''] * 16
+    _buffer_idx = 15
+    _status_flag = 1    # 0 = Good, 1 = Bad
+
+    #constructor
+    def __init__(self,port,print_opt=None):
+        self._port = port
+        if print_opt is None:
+            self._print_option = 0
+        elif int(print_opt) >= 0 and int(print_opt) < 3:
+             self._print_option = print_opt
+        else:
+            print "(SD_4023): Error: print_opt invalid"
+
+    def open(self):
+        if (self._print_option): print "(SD_4023): Initializing device"
+        self._serialObj.baudrate = self._baudrate
+        self._serialObj.port = self._port
+        self._serialObj.bytesize = serial.EIGHTBITS
+        self._serialObj.parity = serial.PARITY_NONE
+        self._serialObj.stopbits = serial.STOPBITS_ONE
+        self._serialObj.timeout = 0.3
+        self._serialObj.write_timeout = 0.3
+
+        try:
+            self._serialObj.open()
+            if (self._print_option): print ("(SD_4023): Serial connection established")
+            self._status_flag = 0
+
+        except serial.serialutil.SerialException:
+            #logsd.ERROR('Could not open serial port %s' % self._port)
+            if (self._print_option): print ("(SD_4023): Could not open serial port %s" % self._port)
+            self._status_flag = 1
+
+    def close(self):
+        try:
+            self._serialObj.close()
+            if (self._print_option): print ("(SD_4023): Successfully closed serial port")
+        except:
+            if (self._print_option): print ("(SD_4023): Could not close port.")
+
+    def read(self):
+        if self._serialObj.is_open == True:
+            if self._status_flag == 0:
+                try:
+                    for i in range(0,16):
+                        self._data_buffer[i] = ''
+                    while (self._buffer_idx >= 0):
+                        byte_in = self._serialObj.read()
+                        self._data_buffer[self._buffer_idx] = byte_in
+                        self._buffer_idx = self._buffer_idx - 1
+                    self._buffer_idx = 15
+                    #logsd.info(self._data_buffer)
+                    if self._data_buffer[0] == '':
+                        raise ValueError("(SD_4023): TimeoutError! Could not read any data")
+                    return self._data_buffer
+                except ValueError as err:
+                    print (err)
+                    self._status_flag = 1
+                    return None
+                except serial.serialutil.SerialException:
+                    if (self._print_option): print "(SD_4023): SERIAL EXCEPTION! Could not read data. Close and reopen the serial connection."
+                    self._status_flag = 1
+
+            else:
+                if (self._print_option): print "(SD_4023): Status flag == 1: Cannot perform read. Close and open serial port again."
+        else:
+            if (self._print_option): print "(SD_4023): Serial port not open. Call method open()"
+            return None
+
+    def read_decibel(self):
+        if self._serialObj.is_open == True:
+            if self._status_flag == 0:
+                try:
+                    self.read()
+                    for i in range(0,16):
+                        if self._data_buffer[i] == '':
+                            return None
+                    temp_str = self._data_buffer[4] + self._data_buffer[3] + self._data_buffer[2] + '.' + self._data_buffer[1]
+                    return float(temp_str)
+                except serial.serialutil.SerialException:
+                    if (self._print_option): print "(SD_4023): Could not read data. Close and reopen the serial connection."
+                    self._status_flag = 1
+            else:
+                if (self._print_option): print "(SD_4023): Status flag = 1: Cannot perform read. Close and open serial port again."
+        else:
+            if (self._print_option): print "(SD_4023): Serial port not open. Call method open()"
+            return None
+
+    def get_status(self):
+        return self._status_flag
